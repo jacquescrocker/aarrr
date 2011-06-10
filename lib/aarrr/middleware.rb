581e5077425b4d834622efce3c1425508c169f36
Jacques Crocker <railsjedi@gmail.com>
2011-06-10 13:53:07 -0700

    getting rid of the middleware for now (it screws up rack cookies)

--- lib/aarrr/middleware.rb
+++ lib/aarrr/middleware.rb
@@ -1,21 +1,22 @@
-module AARRR
-  class Middleware
-
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      aarrr_session = AARRR(env)
-
-      status, headers, body = @app.call(env)
-
-      # sets a tracking cookie on the response
-      response = Rack::Response.new body, status, headers
-      aarrr_session.set_cookie(response)
-
-      response.finish
-    end
-
-  end
-end
+# DONT USE THIS, ITS FUCKED
+# module AARRR
+#   class Middleware
+#
+#     def initialize(app)
+#       @app = app
+#     end
+#
+#     def call(env)
+#       aarrr_session = AARRR(env)
+#
+#       status, headers, body = @app.call(env)
+#       # require 'ruby-debug';debugger
+#       # sets a tracking cookie on the response
+#       response = Rack::Response.new body, status, headers
+#       # aarrr_session.set_cookie(response)
+#
+#       response.finish
+#     end
+#
+#   end
+# end
