72cf2ac5beb3158d580f73fe49e84e042f099409
Alex Sharp <ajsharp@gmail.com>
2011-06-11 15:31:46 -0700

    lets give this middleware another shot.

--- lib/aarrr/middleware.rb
+++ lib/aarrr/middleware.rb
@@ -1,22 +1,23 @@
 # DONT USE THIS, ITS FUCKED
-# module AARRR
-#   class Middleware
-#
-#     def initialize(app)
-#       @app = app
-#     end
-#
-#     def call(env)
-#       aarrr_session = AARRR(env)
-#
-#       status, headers, body = @app.call(env)
-#       # require 'ruby-debug';debugger
-#       # sets a tracking cookie on the response
-#       response = Rack::Response.new body, status, headers
-#       # aarrr_session.set_cookie(response)
-#
-#       response.finish
-#     end
-#
-#   end
-# end
+
+module AARRR
+  class Middleware
+
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      aarrr_session = AARRR(env)
+
+      status, headers, body = @app.call(env)
+      # require 'ruby-debug';debugger
+      # sets a tracking cookie on the response
+      response = Rack::Response.new body, status, headers
+      aarrr_session.set_cookie(response)
+
+      response.finish
+    end
+
+  end
+end
